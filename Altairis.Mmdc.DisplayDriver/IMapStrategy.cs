fb7c36d3f292878c454553d1c53e744d7944e001
Michal Altair Valášek <michal.valasek@altairis.cz>
2018-12-30 01:04:52 +0100

    Added logical display map and their generation

---
+++ Altairis.Mmdc.DisplayDriver/IMapStrategy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Altairis.Mmdc.DisplayDriver {
+    public interface IMapStrategy {
+        PhysicalDisplayPosition GetDisplayPosition(PhysicalDisplayInfo previousInfo, PhysicalDisplayPosition previousPosition);
+    }
+
+    public class HorizontalMapStrategy : IMapStrategy {
+        public PhysicalDisplayPosition GetDisplayPosition(PhysicalDisplayInfo previousInfo, PhysicalDisplayPosition previousPosition) {
+            if (previousInfo == null || previousPosition == null) return new PhysicalDisplayPosition { X = 0, Y = 0 };
+            return new PhysicalDisplayPosition {
+                X = previousPosition.X + previousInfo.Width,
+                Y = previousPosition.Y
+            };
+        }
+    }
+
+    public class VerticalMapStrategy : IMapStrategy {
+        public PhysicalDisplayPosition GetDisplayPosition(PhysicalDisplayInfo previousInfo, PhysicalDisplayPosition previousPosition) {
+            if (previousInfo == null || previousPosition == null) return new PhysicalDisplayPosition { X = 0, Y = 0 };
+            return new PhysicalDisplayPosition {
+                X = previousPosition.X,
+                Y = previousPosition.Y + previousInfo.Height
+            };
+        }
+    }
+
+}
---
+++ Altairis.Mmdc.DisplayDriver/LogicalDisplayMap.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Altairis.Mmdc.DisplayDriver {
+    public class LogicalDisplayMap : Dictionary<PhysicalDisplayInfo, PhysicalDisplayPosition> {
+
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public static LogicalDisplayMap Create(IEnumerable<PhysicalDisplayInfo> displays, IMapStrategy strategy) {
+            if (displays == null) throw new ArgumentNullException(nameof(displays));
+            if (strategy == null) throw new ArgumentNullException(nameof(strategy));
+            if (!displays.Any()) throw new ArgumentException("At least one display must be supplied.", nameof(displays));
+
+            // Create empty map
+            var map = new LogicalDisplayMap();
+            PhysicalDisplayInfo lastDisplay = null;
+            PhysicalDisplayPosition lastPosition = null;
+
+            // Layout display using specified strategy
+            foreach (var display in displays) {
+                lastPosition = strategy.GetDisplayPosition(lastDisplay, lastPosition);
+                lastDisplay = display;
+                map.Add(lastDisplay, lastPosition);
+            }
+
+            // Compute size
+            map.Width = map.Max(d => d.Value.X + d.Key.Width);
+            map.Height = map.Max(d => d.Value.Y + d.Key.Height);
+
+            return map;
+        }
+
+        public static LogicalDisplayMap LoadFromFile(string fileName) => LoadFromJson(File.ReadAllText(fileName));
+
+        public static LogicalDisplayMap LoadFromJson(string json) {
+            if (json == null) throw new ArgumentNullException(nameof(json));
+            if (string.IsNullOrWhiteSpace(json)) throw new ArgumentException("Value cannot be empty or whitespace only string.", nameof(json));
+
+            return JsonConvert.DeserializeObject<LogicalDisplayMap>(json);
+        }
+
+        public string SaveToJson(Formatting formatting = Formatting.Indented) => JsonConvert.SerializeObject(this, formatting);
+
+        public void SaveToFile(string fileName, Formatting formatting = Formatting.Indented) => File.WriteAllText(fileName, this.SaveToJson(formatting));
+
+    }
+}
