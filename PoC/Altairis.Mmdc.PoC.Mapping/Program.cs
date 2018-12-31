462faa92d8335f4339505003f4ab5240d59cfd58
Michal Altair Valášek <michal.valasek@altairis.cz>
2018-12-31 18:39:57 +0100

    Added mapping PoC

---
+++ PoC/Altairis.Mmdc.PoC.Mapping/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Altairis.Mmdc.PoC.Mapping {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
