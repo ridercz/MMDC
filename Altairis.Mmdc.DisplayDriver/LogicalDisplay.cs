cab3c8c6cd6bfcaef5a03a0fd2095966712ecfd6
Michal Altair Valášek <michal.valasek@altairis.cz>
2019-01-01 20:43:34 +0100

    Added logical display capability.

---
+++ Altairis.Mmdc.DisplayDriver/LogicalDisplay.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.Primitives;
+
+namespace Altairis.Mmdc.DisplayDriver {
+    public class LogicalDisplay : DisplayBase {
+        private readonly DisplayMap map;
+        private readonly List<PhysicalDisplay> displays = new List<PhysicalDisplay>();
+        private bool isOpen = false;
+
+        public LogicalDisplay(DisplayMap map) {
+            this.map = map;
+        }
+
+        public override int Width => this.map.Width;
+
+        public override int Height => this.map.Height;
+
+        public void Open() {
+            this.displays.Clear();
+            foreach (var item in this.map.Items) {
+                var newDisplay = new PhysicalDisplay(item.Display.PortName);
+                newDisplay.Open();
+                this.displays.Add(newDisplay);
+            }
+            this.isOpen = true;
+        }
+
+        public void Close() {
+            foreach (var item in this.displays) {
+                item.Close();
+            }
+            this.displays.Clear();
+            this.isOpen = false;
+        }
+
+        public override void SendFrame(byte[] rawData) {
+            if (rawData == null) throw new ArgumentNullException(nameof(rawData));
+            if (!this.isOpen) throw new InvalidOperationException("Display not open.");
+
+            // Compute and compare expected frame length
+            var frameLength = this.Width * this.Height * 3;
+            if (rawData.Length != frameLength) throw new ArgumentException($"Invalid data length. Expected {frameLength} bytes, got {rawData.Length} bytes.", nameof(rawData));
+
+            // Load raw data as image
+            var img = Image.LoadPixelData<Rgb24>(rawData, this.Width, this.Height);
+
+            // Send image
+            this.SendFrame(img);
+        }
+
+        public override void SendFrame<TPixel>(Image<TPixel> image) {
+            if (image == null) throw new ArgumentNullException(nameof(image));
+            if (!this.isOpen) throw new InvalidOperationException("Display not open.");
+
+            foreach (var display in this.displays) {
+                // Get display rectangle
+                var mapItem = this.map.Items.FirstOrDefault(x => x.Display.SerialNumber == display.Properties.SerialNumber);
+                var displayRectangle = new Rectangle {
+                    X = mapItem.Position.Y,
+                    Y = mapItem.Position.Y,
+                    Width = mapItem.Display.Width,
+                    Height = mapItem.Display.Height
+                };
+
+                // Get cropped image
+                var croppedImage = image.Clone(x => x.Crop(displayRectangle));
+
+                // Send cropped image
+                display.SendFrame(croppedImage);
+            }
+
+        }
+
+
+    }
+}
---
+++ PoC/Altairis.Mmdc.PoC.Logical/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using Altairis.Mmdc.DisplayDriver;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace Altairis.Mmdc.PoC.Logical {
+    internal class Program {
+        private static void Main(string[] args) {
+            // List all available displays
+            Console.WriteLine("Let's look for displays. It may take several minutes to complete.");
+            Console.Write("Scanning ports for compatible displays...");
+            var displays = PhysicalDisplayManager.ScanPorts();
+            if (displays.Count == 0) {
+                Console.WriteLine("Failed!");
+                Console.WriteLine("No displays found.");
+                return;
+            }
+            Console.WriteLine($"OK, found {displays.Count} displays.");
+
+            // Create display map
+            Console.Write("Creating display map...");
+            var map = DisplayMap.Create(displays, new VerticalMapBuildStrategy());
+            Console.WriteLine($"OK, size {map.Width} x {map.Height}");
+
+            // Create logical display
+            Console.Write("Creating and connecting logical display...");
+            var display = new LogicalDisplay(map);
+            display.Open();
+            Console.WriteLine("OK");
+
+            // Create image
+            Console.WriteLine("Sending image:");
+            var image = new Image<Rgb24>(display.Width, display.Height);
+            for (var x = 0; x < image.Width; x++) {
+                for (var y = 0; y < image.Height; y++) {
+                    Console.Write(".");
+                    image[x, y] = new Rgb24(0x33, 0x00, 0x00); // Red
+                    display.SendFrame(image);
+                    //Thread.Sleep(10);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
